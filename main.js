da15f1633149f26a39740792ecf899be59e1243e
BarZ <andy820621@gmail.com>
2022-08-19 16:46:19 +0900

    add: use a11y to design slider

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import A11YSlider from "./node_modules/a11y-slider";
+import A11Css from "./node_modules/a11y-slider/dist/a11y-slider.css";
+
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
@@ -18,3 +21,24 @@ navToggle.addEventListener("click", (e) => {
 primaryHeader.addEventListener("click", () => {
 	if (primaryNav.hasAttribute("data-visible")) toggleMenu();
 });
+
+// A11y Slider
+const slider = new A11YSlider(document.querySelector(".slider"), {
+	slidesToShow: 1,
+	arrows: false,
+	centerMode: true,
+	dots: true,
+	responsive: {
+		800: {
+			arrows: true,
+			slidesToShow: 2,
+			dots: false,
+		},
+		1280: {
+			slidesToShow: 3,
+		},
+	},
+});
+document
+	.querySelectorAll(".a11y-slider-container button:not([tabindex])")
+	.forEach((btn) => (btn.textContent = ""));
